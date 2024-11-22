69f149c0130020ce21e7b5ff70588c23b4989a56
Aashey <dazewaker12345@gmail.com>
2024-11-22 21:49:53 +0545

    Integrated ImageKit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { IKImage } from "imagekitio-react";
+import Image from "./Image";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
 
-    const [open, setOpen] = useState(false);
-
-    return (
-        <div className='w-full h-16 md:h-20 flex items-center justify-between'>
-            <div className="flex items-center gap-4 text-2xl font-bold">
-                LOGO
-                <span>BLOG APP</span>
-            </div>
-            {/* Mobile Menu */}
-            <div className="md:hidden">
-                <div className="cursor-pointer text-2xl" onClick={() => setOpen((prev) => !prev)}>{open ? "✕" : "☰"}</div>
-                <div className="">
-                   {/* Mobile Link List   */}
-                   <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e6e6ff] transition-all ease-out ${open ? "-right-0" : "-right-[100%]"}`}>
-                   <a href="">Home</a>
-                <a href="">Trending</a>
-                <a href="">Most Popular</a>
-                <a href="">About</a>
-                <a href="">
-                    <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login</button>
-                </a>
-                   </div>
-                </div>
-            </div>
-            {/* Desktop Menu */}
-            <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-                <a href="">Home</a>
-                <a href="">Trending</a>
-                <a href="">Most Popular</a>
-                <a href="">About</a>
-                <a href="">
-                    <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>Login</button>
-                </a>
-            </div>
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <Image src={"logo.png"} alt="Logo" w={32} h={32} />
+        <span>BLOG APP</span>
+      </div>
+      {/* Mobile Menu */}
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer text-2xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "✕" : "☰"}
         </div>
-    )
-}
+        <div className="">
+          {/* Mobile Link List   */}
+          <div
+            className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-[#e6e6ff] transition-all ease-out ${open ? "-right-0" : "-right-[100%]"}`}
+          >
+            <a href="">Home</a>
+            <a href="">Trending</a>
+            <a href="">Most Popular</a>
+            <a href="">About</a>
+            <a href="">
+              <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+                Login
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Desktop Menu */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="">Home</a>
+        <a href="">Trending</a>
+        <a href="">Most Popular</a>
+        <a href="">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+            Login
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
